5c24de7226db2a080951d443ab873173ceb40a43
hoangnn198 <66403260+hoangnn198@users.noreply.github.com>
2020-12-08 00:09:31 +0700

    cm

--- src/App.js
+++ src/App.js
@@ -1,36 +1,73 @@
-// import logo from './logo.svg';
-import './App.css';
-import TopNav from "./components/custom/TopNavComponents";
+import React, { useState } from 'react';
+// import backgr1 from "./res/image/backgr1.jpg";
 import Slider from "./components/custom/SliderComponents";
-import backgr1 from "./res/image/backgr1.jpg";
+import ic_menu from "./res/image/ic_menu.png";
+import ic_search from "./res/image/ic_search.png";
+import './App.css'
 
-function App() {
+const App = () => {
+  const [checkMenu, setCheckMenu] = useState(false)
   return (
-    <div className="App">
+    <div >
 
       {/* header  */}
-      <div className="header">
-        <TopNav />
-      </div>
-      {/* body  */}
-      <div className="hideHeader" />
+      <div className="header" >
+        <div style={{ width: '40%', display: 'flex', flexDirection: 'row', height: '100%' }}>
+          <div className="ic_menuView" onClick={() => setCheckMenu(!checkMenu)} >
+            <img src={ic_menu} alt="" style={{ height: 35, width: 35 }} />
 
-      <div className="body">
-        <div >
-          <Slider />
+          </div>
+          <div style={{ width: '80%', height: '100%', backgroundColor: 'lavender', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+            <h5>Logo</h5>
+          </div>
         </div>
 
+        {/* search  */}
+        <div className="ic_searchView">
+          <img src={ic_search} className="ic_search" alt="search" />
+        </div>
+      </div>
+      {checkMenu &&
+        <div className="menuView" onClick={() => setCheckMenu(false)}>
+          <div className="menu" >
 
+          </div>
+        </div>
+      }
+      <div className="divHide" />
+
+      {/* slider  */}
+      <div className="slider">
+        <Slider />
       </div>
 
-      {/* header  */}
-      <div className="footer">
+      {/* body */}
+      <div className="body">
+        <div className="products">
+          {/* <h4 style={{ marginTop: 20, textAlign: 'center' }}>PRODUCTS</h4> */}
 
+          <div style={{ backgroundColor: 'cadetblue', height: 200, width: '100%', display: 'flex', flexDirection: 'row', flexWrap: 'wrap' }}>
+            <div class="col-6  col-xl-3" style={{ height: '200px', backgroundColor: 'red', }}>
+              1 of 2
+           </div>
+            <div class="col-6 col-xl-3" style={{ height: '200px', backgroundColor: 'yellowgreen', }}>
+              2 of 2
+            </div>
+            <div class="col-6 col-xl-3" style={{ height: '200px', backgroundColor: 'red', }}>
+              1 of 2
+           </div>
+            <div class="col-6 col-xl-3" style={{ height: '200px', backgroundColor: 'yellowgreen', }}>
+              2 of 2
+            </div>
+
+          </div>
+        </div>
       </div>
-
 
     </div>
   );
 }
 
-export default App;
+
+
+export default App
--- src/components/custom/SliderComponents.js
+++ src/components/custom/SliderComponents.js
@@ -1,39 +1,88 @@
-import React, { Component } from 'react';
-import '../../css/slider.css'
-import backgr1 from '../../res/image/backgr1.jpg'
+import React, { useState } from 'react';
+import {
+    Carousel,
+    CarouselItem,
+    CarouselControl,
+    CarouselIndicators,
+    CarouselCaption
+} from 'reactstrap';
+import backgr1 from "../../res/image/backgr1.jpg";
+import "../../css/slider.css";
+const items = [
+    {
+        src: backgr1,
+        altText: 'Slide 1',
+        caption: 'Slide 1'
+    },
+    {
+        src: backgr1,
+        altText: 'Slide 2',
+        caption: 'Slide 2'
+    },
+    {
+        src: backgr1,
+        altText: 'Slide 3',
+        caption: 'Slide 3'
+    }
+];
 
-export default class Slider extends Component {
-    render() {
+const Example = (props) => {
+    const [activeIndex, setActiveIndex] = useState(0);
+    const [animating, setAnimating] = useState(false);
+
+    const next = () => {
+        if (animating) return;
+        const nextIndex = activeIndex === items.length - 1 ? 0 : activeIndex + 1;
+        setActiveIndex(nextIndex);
+    }
+
+    const previous = () => {
+        if (animating) return;
+        const nextIndex = activeIndex === 0 ? items.length - 1 : activeIndex - 1;
+        setActiveIndex(nextIndex);
+    }
+
+    const goToIndex = (newIndex) => {
+        if (animating) return;
+        setActiveIndex(newIndex);
+    }
+
+    const slides = items.map((item) => {
         return (
-            <div className="slider">
-                <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel" style={{ height: '100%', width: '100%' }} >
-                    <ol className="carousel-indicators">
-                        <li data-target="#carouselExampleIndicators" data-slide-to={0} className="active" />
-                        <li data-target="#carouselExampleIndicators" data-slide-to={1} />
-                        <li data-target="#carouselExampleIndicators" data-slide-to={2} />
-                    </ol>
-                    <div className="carousel-inner" style={{ height: '100%', width: '100%' }} >
-                        <div className="carousel-item active" style={{ height: '100%', width: '100%' }} >
-                            <img src={backgr1} style={{ height: '100%', width: '100%' }} />
-                        </div>
-                        <div className="carousel-item">
-                            <img src={backgr1} style={{ height: '100%', width: '100%' }} />
-                        </div>
-                        <div className="carousel-item">
-                            <img src={backgr1} style={{ height: '100%', width: '100%' }} />
-                        </div>
-                    </div>
-                    <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                        <span className="carousel-control-prev-icon" aria-hidden="true" />
-                        <span className="sr-only">Previous</span>
-                    </a>
-                    <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                        <span className="carousel-control-next-icon" aria-hidden="true" />
-                        <span className="sr-only">Next</span>
-                    </a>
+
+            <CarouselItem
+                onExiting={() => setAnimating(true)}
+                onExited={() => setAnimating(false)}
+                key={item.src}
+            >
+                <div className="imgView">
+                    <img src={item.src} alt={item.altText} style={{ height: '100%', width: '100%' }} />
                 </div>
 
-            </div>
+                <CarouselCaption captionText={item.caption} captionHeader={item.caption} />
+            </CarouselItem>
+
+
         );
-    }
+    });
+
+    return (
+        <div style={{ height: '100%', width: '100%' }} >
+            <Carousel
+                autoPlay={true}
+                interval={2000}
+                activeIndex={activeIndex}
+                next={next}
+                previous={previous}
+            >
+                <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={goToIndex} />
+                {slides}
+                <CarouselControl direction="prev" directionText="Previous" onClickHandler={previous} />
+                <CarouselControl direction="next" directionText="Next" onClickHandler={next} />
+            </Carousel>
+        </div>
+
+    );
 }
+
+export default Example;
